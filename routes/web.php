3385d0aa043004916420cdc9b55e0590dcea5975
Hanae05 <chacopipi0516@icloud.com>
2020-04-26 17:24:20 +0900

    chapter3まで進んだぞ〜

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('hello', 'HelloController@index');
+Route::post('hello', 'HelloController@post');
