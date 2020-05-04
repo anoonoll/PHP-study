aafcd5d8658f8dff006791fed459304dce14c1ce
Hanae05 <chacopipi0516@icloud.com>
2020-05-04 22:30:58 +0900

    242page

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -13,8 +13,8 @@ class HelloController extends Controller
 
     public function index(Request $request) 
     {
-        $items = DB::select('select * from people');
-        return view('hello.index', ['items' => $items]);
+        $items = DB::table('people')->get();
+        return view('hello.index', ['items'=> $items]);
     }
 
     public function post(Request $request)
@@ -27,5 +27,60 @@ class HelloController extends Controller
         $response = response()->view('hello.index', ['msg'=>'「' . $msg . '」をクッキーに保存しました。']);
         $response->cookie('msg', $msg, 100);
         return $response;
+    }
+
+    public function add(Request $request)
+    {
+        return view('hello.add');
+    }
+
+    public function create(Request $request)
+    {
+        $param = [
+            'name' => $request->name,
+            'mail' => $request->mail,
+            'age' => $request->age,
+        ];
+        DB::table('people')->insert($param);
+        return redirect('/hello');
+    }
+
+    public function edit(Request $request)
+    {
+        $item = DB::table('people')->where('id', $request->id)->first();
+
+        return view('hello.edit', ['form' => $item]);
+    }
+
+    public function update(Request $request)
+    {
+        $param = [
+            'name' => $request->name,
+            'mail' => $request->mail,
+            'age' => $request->age,
+        ];
+        DB::table('people')->where('id', $request->id)->update($param);
+
+        return redirect('/hello');
+    }
+
+    public function del(Request $request)
+    {
+        $item = DB::table('people')->where('id', $request->id)->first();
+        return view('hello.del', ['form' => $item]);
+    }
+
+    public function remove(Request $request)
+    {
+        DB::table('people')->where('id', $request->id)->delete();
+        return redirect('/hello');
+    }
+
+    public function show(Request $request)
+    {
+        $page = $request->page;
+        $items = DB::table('people')->offset($page * 3)->limit(3)->get();
+
+        return view('hello.show', ['items' => $items]);
     }
 }
---
+++ database/seeds/PeopleTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PeopleTableSeeder extends Seeder
+{
+    public function run()
+    {
+        $param = [
+            'name' => 'taro',
+            'mail' => 'taro@yamada.jp',
+            'age' => 12,
+        ];
+        DB::table('people')->insert($param);
+
+        $param = [
+            'name' => 'hanako',
+            'mail' => 'hanako@flower',
+            'age' => 34,
+        ];
+        DB::table('people')->insert($param);
+
+        $param = [
+            'name' => 'sachiko',
+            'mail' => 'sachiko@happy.jp',
+            'age' => 56, 
+        ];
+        DB::table('people')->insert($param);
+    }
+}
